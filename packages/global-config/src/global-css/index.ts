a1354ee85ed104130835d03ed77e93adb0ed904d
iWuzhi <2461358597@qq.com>
2021-01-31 01:50:13 +0800

    refactor: Remvoe global config

--- packages/global-config/src/global-css/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * author iWuzhi
- * date 2020-08-24 22:52:20
- */
-
-export default [
-  /* -------------------------------- css 初始化 -------------------------------- */
-  '/global-config/lib/css/normalize.css'
-]
--- packages/global-config/src/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * author iWuzhi
- * date 2020-08-22 13:03:27
- */
-
-export { default as webpackExternals } from './webpack-externals';
-
-export { default as globalCSS } from './global-css';
